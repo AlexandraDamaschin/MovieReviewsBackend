ec89e1492ef19d1d286e639c747cede71ff38cf3
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-02-23 15:14:32 +0000

    models created

---
+++ MovieReviewsBackend/Models/MovieModels/MovieDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Web;
+
+namespace MovieReviewsBackend.Models.MovieModels
+{
+    public class MovieDbContext : DbContext
+    {
+        public MovieDbContext() : base("DefaultConnection")
+        {
+            Configuration.LazyLoadingEnabled = false;
+        }
+        
+        public DbSet<Review> Reviews { get; set; }
+
+    }
+}
---
+++ MovieReviewsBackend/Models/MovieModels/Review.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MovieReviewsBackend.Models.MovieModels
+{
+    [Table("Review")]
+    public class Review
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "Review Id")]
+        public int ReviewId { get; set; }
+
+        [Display(Name = "User Id")]
+        public int UserId { get; set; }
+
+        [Display(Name = "Imdb Id")]
+        public string ImdbId { get; set; }
+
+        [Display(Name = "Review Comment")]
+        public string ReviewComment { get; set; }
+
+        [Column(TypeName = "date")]
+        [Display(Name = "Date Created")]
+        public DateTime? DateCreated { get; set; }
+
+        [Display(Name = "Star Rating")]
+        public int StarRating { get; set; }
+    }
