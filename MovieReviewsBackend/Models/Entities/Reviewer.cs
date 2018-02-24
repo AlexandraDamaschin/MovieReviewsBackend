15b8519359fc8f12d5be85689d1be178d6e10a5f
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-02-24 11:23:49 +0000

    Reviewer class added

---
+++ MovieReviewsBackend/Models/Entities/Reviewer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieReviewsBackend.Models.Entities
+{
+    public class Reviewer
+    {
+        public int Id { get; set; }
+        public string IdentityId { get; set; }
+        public AppUser Identity { get; set; }  // navigation property
+        public string Location { get; set; }
+
+    }
+}
