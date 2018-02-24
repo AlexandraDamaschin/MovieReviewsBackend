ec8e5e917f7013db272c5d7da1aa4b326cadd89d
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-02-24 11:27:23 +0000

    ApplicationDbContext created

---
+++ MovieReviewsBackend/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+
+namespace MovieReviewsBackend.Data
+{
+    public class ApplicationDbContext: IdentityDbContext
+    {
+        public ApplicationDbContext() : base("DefaultConnection")
+        {
+        }
+    
+        public DbSet Reviewer { get; set; }
+    }
+}
