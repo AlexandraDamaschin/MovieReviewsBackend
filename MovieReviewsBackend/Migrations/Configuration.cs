6dc67ac025a952e9357719fd44ac40fefe5fd3a8
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-02-26 11:48:19 +0000

    C:/Program Files/Git/token

--- MovieReviewsBackend/Migrations/Configuration.cs
+++ MovieReviewsBackend/Migrations/Configuration.cs
@@ -1,11 +1,7 @@
 namespace MovieReviewsBackend.Migrations
 {
     using MovieReviewsBackend.Models;
-    using MovieReviewsBackend.Models.MovieModels;
-    using System;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
-    using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<MovieReviewsBackend.Models.ApplicationDbContext>
     {
@@ -20,14 +16,15 @@ namespace MovieReviewsBackend.Migrations
         //seedReviews
         private void SeedUser(ApplicationDbContext context)
         {
-            context.AspNetUsers.AddOrUpdate(u => u.Id, new Review
-            {
-                UserId = 6,
-                ImdbId = "tt0099785",
-                ReviewComment = "uuu",
-                DateCreated = DateTime.Now,
-                StarRating = 5
-            });
+            //context.AspNetUsers.AddOrUpdate(u => u.Id, new Review
+            //{
+            //    UserId = 6,
+            //    ImdbId = "tt0099785",
+            //    ReviewComment = "uuu",
+            //    DateCreated = DateTime.Now,
+            //    StarRating = 5
+            //});
 
         }
     }
+}
--- MovieReviewsBackend/Models/IdentityModels.cs
+++ MovieReviewsBackend/Models/IdentityModels.cs
@@ -1,10 +1,7 @@
-﻿using System.Data.Entity;
-using System.Security.Claims;
+﻿using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
-using Microsoft.AspNet.Identity.Owin;
-using MovieReviewsBackend.Models.Entities;
 
 namespace MovieReviewsBackend.Models
 {
