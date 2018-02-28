4f15406df471a3342d228d0a4914d92a5f104671
Josh Butler <31965183+S00161171@users.noreply.github.com>
2018-02-28 21:04:55 +0000

    chat created

---
+++ MovieReviewsBackend/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieReviewsBackend.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void Announce(string AnnouncementIn)
+        {
+            Clients.All.Announce(AnnouncementIn);
+        }
+    }
+}
