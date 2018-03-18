971ae53762c6b7174547ecd98369c9504d3f1d90
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2018-03-18 18:59:17 +0000

    interface created for review repository

---
+++ MovieReviewsBackend/DAL/IReviewRepository.cs
@@ -0,0 +1,11 @@
+﻿using MovieReviewsBackend.Models.MovieModels;
+using System.Collections.Generic;
+
+namespace MovieReviewsBackend.DAL
+{
+    interface IReviewRepository
+    {
+        List<Review> GetReviews();
+        Review GetReviewById(int id);
+    }
+}
