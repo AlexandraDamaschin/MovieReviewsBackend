cdc45ff21131de94430ef837b51623427c00b8cd
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2018-03-18 20:19:47 +0000

    interface implemented with all methods needed

--- MovieReviewsBackend/DAL/IReviewRepository.cs
+++ MovieReviewsBackend/DAL/IReviewRepository.cs
@@ -4,9 +4,16 @@ using System.Collections.Generic;
 
 namespace MovieReviewsBackend.DAL
 {
-    interface IReviewRepository 
+    interface IReviewRepository
     {
         List<Review> GetReviews();
         Review GetReviewById(int id);
+        Review GetReviewMovieById(string imdbId);
+
+        void CreateReview(Review review);
+        void UpdateReview(Review review);
+        void DeleteReview(int revireId);
+
+        void Save();
     }
 }
