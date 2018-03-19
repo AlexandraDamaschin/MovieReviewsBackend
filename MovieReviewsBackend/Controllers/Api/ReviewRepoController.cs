d71cba457bec9cb77ad53f8187dc39b54992d881
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2018-03-19 17:39:48 +0000

    get reviewRepo is working

--- MovieReviewsBackend/Controllers/Api/ReviewRepoController.cs
+++ MovieReviewsBackend/Controllers/Api/ReviewRepoController.cs
@@ -20,13 +20,49 @@ namespace MovieReviewsBackend.Controllers.Api
             reviewRepo = new DAL.ReviewRepository(new MovieDbContext());
         }
 
-        //GET /api/Reviews
+        //GET /api/ReviewRepo
         //get all reviews
         public List<Review> GetReviews()
         {
             return reviewRepo.GetReviews();
         }
 
+        //GET /api/ReviewRepo/1
+        public Review GetReviewById(int reviewId)
+        {
+            return reviewRepo.GetReviewById(reviewId);
+        }
 
+        //GET /api/ReviewRepo/imdbid
+        public Review GetReviewMovieById(string imdbId)
+        {
+            return reviewRepo.GetReviewMovieById(imdbId);
+        }
+
+        //POST /api/ReviewRepo
+        public void CreateReview(Review review)
+        {
+            reviewRepo.CreateReview(review);
+
+        }
+
+        //PUT /api/ReviewRepo/1
+        public void UpdateReview(int reviewId)
+        {
+            reviewRepo.UpdateReview(reviewId);
+        }
+
+        //DELETE /api/ReviewRepo/1
+
+        public void DeleteReview(int reviewId)
+        {
+            reviewRepo.DeleteReview(reviewId);
+        }
+
+        //save changes
+        public void Save()
+        {
+            reviewRepo.Save();
+        }
     }
 }
