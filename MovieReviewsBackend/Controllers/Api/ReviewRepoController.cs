294f24a09d650393ca200cfba8254c1e2c99e7ce
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2018-03-19 17:56:53 +0000

    by id ?

--- MovieReviewsBackend/Controllers/Api/ReviewRepoController.cs
+++ MovieReviewsBackend/Controllers/Api/ReviewRepoController.cs
@@ -14,6 +14,8 @@ namespace MovieReviewsBackend.Controllers.Api
         //set context to repo
         private ReviewRepository reviewRepo;
 
+        private MovieDbContext _movie;
+
         //constructor 
         public ReviewRepoController()
         {
@@ -30,25 +32,28 @@ namespace MovieReviewsBackend.Controllers.Api
         //GET /api/ReviewRepo/1
         public Review GetReviewById(int reviewId)
         {
+            var review = _movie.Reviews.SingleOrDefault(r => r.ReviewId == reviewId);
             return reviewRepo.GetReviewById(reviewId);
         }
 
         //GET /api/ReviewRepo/imdbid
         public Review GetReviewMovieById(string imdbId)
         {
+            var review = _movie.Reviews.ToList().Where(r => r.ImdbId == imdbId);
             return reviewRepo.GetReviewMovieById(imdbId);
         }
 
         //POST /api/ReviewRepo
         public void CreateReview(Review review)
         {
+            _movie.Reviews.Add(review);
             reviewRepo.CreateReview(review);
-
         }
 
         //PUT /api/ReviewRepo/1
         public void UpdateReview(int reviewId)
         {
+            var reviewInDb = _movie.Reviews.SingleOrDefault(r => r.ReviewId == reviewId);
             reviewRepo.UpdateReview(reviewId);
         }
 
@@ -56,6 +61,7 @@ namespace MovieReviewsBackend.Controllers.Api
 
         public void DeleteReview(int reviewId)
         {
+            var reviewInDb = _movie.Reviews.SingleOrDefault(r => r.ReviewId == reviewId);
             reviewRepo.DeleteReview(reviewId);
         }
 
